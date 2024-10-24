6c47aa3f895680120155dc0abdbdebe2631f32f7
Stanislaw-K <nightelf2293@gmail.com>
2024-10-24 19:31:26 +0200

    hanson module completed

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -15,6 +15,20 @@ export default class Slider {
             this.slideIndex = this.slides.length;
         }
 
+        try {
+            this.hanson.style.opacity = '0';
+
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch (e) { }
+
         Array.from(this.slides).forEach(slide => {
             slide.style.display = 'none';
         });
@@ -30,9 +44,19 @@ export default class Slider {
     }
 
     render() {
+        try {
+            this.hanson = document.querySelector('.hanson');
+        } catch (e) { }
+
         this.btns.forEach(item => {
             item.addEventListener('click', () => {
                 this.plusSlides(1);
+            });
+
+            item.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.slideIndex = 1;
+                this.showSlides(this.slideIndex);
             });
 
             item.parentNode.previousElementSibling.addEventListener('click', (e) => {
